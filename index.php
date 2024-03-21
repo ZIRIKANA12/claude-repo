7cdbed8ae42cc0fb280cd7306aae36f6e11d05fe
Emmy jojoly <emmyjojoly@gmail.com>
2024-03-21 02:14:10 -0700

    index page is created

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "WELCOME TO IPRC TUMBA";
+?>
+<html>
+    <head><title>firstpage</title>
+</body>
+<h1>DEVELOPMENT OPERATIONS</h1>
+<a href="login.php">LOGIN PAGE</a><br>
+<a href="registration.php">LOGIN PAGE</a>
+</body>
+</html>
